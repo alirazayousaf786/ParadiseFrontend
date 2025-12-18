a7d86f3e7c3c97ab7f1670b2be04cdf4d7dc3296
Ali Raza <alirazayousaf420@gmail.com>
2025-12-18 10:47:26 +0500

    change url

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Hero from "../component/Hero"
-import HeroLast from "../component/HeroLast"
-import Bunner from "../component/Bunner"
-import ImageGallery from "../component/ImageGallery"
-import Textimonial from "../component/Textimonial"
-import According from "../component/According"
-import Contact from "../component/Contact"
-import LastPrium from "../component/LastPrium"
-import Product from "../component/Product"
+import Hero from "../component/hero.jsx";
+import HeroLast from "../component/herolast.jsx";
+import Bunner from "../component/bunner.jsx";
+import ImageGallery from "../component/imagegallery.jsx";
+import Textimonial from "../component/textimonial.jsx";
+import According from "../component/according.jsx";
+import Contact from "../component/contact.jsx";
+import LastPrium from "../component/lastprium.jsx";
+import Product from "../component/product.jsx";
 
 export default function Home() {
   return (
@@ -22,5 +21,5 @@ export default function Home() {
       <Contact />
       <LastPrium />
     </>
-  )
+  );
 }
